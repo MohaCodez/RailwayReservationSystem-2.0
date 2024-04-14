9f984416182463ae3c384151144fb028fe7d66ba
Deven <f20212821@goa.bits-pilani.ac.in>
2024-04-14 20:12:26 +0530

    implemented Main.java

--- java-files/Main.java
+++ java-files/Main.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-    	
-    	Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int choice;
         do {
             System.out.println("Railway Reservation System Menu:");
@@ -15,11 +13,30 @@ public class Main {
             System.out.println("[4] Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
-            
+
             switch (choice) {
                 case 1:
-                    // Handle booking ticket
-                    // Example: bookingSystem.bookTicket();
+                    System.out.print("Enter your full name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter your age: ");
+                    int age = scanner.nextInt();
+                    System.out.print("Enter your phone number: ");
+                    String phnNo = scanner.nextLine();
+                    System.out.print("Enter your email ID: ");
+                    String emailID = scanner.nextLine();
+                    System.out.print(
+                            "Do you belong to any of these categories: Student/Senior/Military/Disabled?\n(Y/N): ");
+                    String isSpecialCategory = scanner.nextLine().toLowerCase();
+
+                    if (isSpecialCategory.equals("y")) {
+                        System.out.print("Enter your category(Student/Senior/Military/Disabled):");
+                        String specialCategory = scanner.nextLine().toLowerCase();
+                        Passenger passenger = new Passenger(name, age, phnNo, emailID, specialCategory);
+
+                    } else {
+                        Passenger passenger = new Passenger(name, age, phnNo, emailID);
+                    }
+
                     break;
                 case 2:
                     // Handle canceling ticket
@@ -37,5 +54,6 @@ public class Main {
                     break;
             }
         } while (choice != 4);
+        scanner.close();
     }
 }
